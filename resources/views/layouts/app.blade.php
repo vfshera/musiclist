c5bcadd35010160c3145150863fe06c74d69bc9b
FranklinShera <fshera96@gmail.com>
2021-02-13 20:01:28 +0300

    SEO META

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The Official website for Producer Tony Gregory aka TonyOnTheTrack! | Get Your Bangers Here! | Gengetone, Drill, Hip Hop, Afrobeats">
+    <meta name="keywords" content="Producer, Beat Maker, Beats, TonyOnTheTrack,Tony Gregory,Gengetone, Drill, Hip Hop, Kenya Beats,Afrobeats,Music">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Tony On The Track | Tony Gregory') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
