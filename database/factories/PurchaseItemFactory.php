34e5a9ac58f88d64b9cf4efc99fa37d2b9acf92e
FranklinShera <fshera96@gmail.com>
2021-02-20 04:16:38 +0300

    Purchase Item Created

---
+++ database/factories/PurchaseItemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PurchaseItem;
+use Faker\Generator as Faker;
+
+$factory->define(PurchaseItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
